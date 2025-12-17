c953825b845d8fb48cce5bb285688ea9dad76ac7
rodelcoder1 <rodelpellazar1@gmail.com>
2025-12-18 00:30:01 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = readme_path.read_text(encoding="utf-8")  # <-- UTF-8 here
 
 setup(
     name="assumphelp",
-    version="0.1.6",
+    version="0.1.0",
     packages=["assumphelp"],
     install_requires=[
         "numpy>=1.24.0",
