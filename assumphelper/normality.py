2c1260ee7ecb225516d70dd7359281e5a5e0a1c6
rodelcoder1 <rodelpellazar1@gmail.com>
2025-12-19 01:44:31 +0800

    Update normality.py

--- assumphelper/normality.py
+++ assumphelper/normality.py
@@ -11,13 +11,16 @@ class Normality(Hypothesis):
     def fit(self):
         self.model.fit(self.x_cons, self.y)
         self.fitted, self.residuals = prepare_vars(self.model, self.x_cons, self.y)
+        self.fit_done = True
         return self
    
     def test(self):
         """
         Perform Shapiro-Wilk test.
         """
-        self.fit()
+         if not getattr(self, "fit_done", False):
+            raise NotFittedError("Call fit() before test().")
+             
         shapiro_stat, shapiro_pval = sp.shapiro(self.residuals)
         self.result = shapiro_pval
         print("Shapiro-Wilk Test for Normality")
